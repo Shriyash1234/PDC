70f5e0613d2f136137feb780d3489c8b5892837c
Rahul Raj <raj.rahul@iitgn.ac.in>
2023-04-16 11:02:27 +0530

    Create clubs.js

---
+++ src/components/clubs.js
@@ -0,0 +1,53 @@
+import Header from "./header";
+import Footer from './footer';
+import './CSS/clubs.css';
+import { useState } from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import { Container, Row, Col } from "react-bootstrap";
+// import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
+function Clubs() {
+
+    return (
+        <div>
+            <Header />
+
+            <div className="container">
+                <h1>Annuity | Finance Club</h1>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <img
+                                    src="https://scontent-bom1-2.xx.fbcdn.net/v/t39.30808-6/309439876_420567350237561_2156672749790611013_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=3ljSdv1ozq0AX98PGCz&_nc_oc=AQnMijPukW5wyFILJHjUwYQ-RA9IJQq5YAAd7cnFHKeHCVNiWEzKsBvjBU7LSt8kykA&_nc_ht=scontent-bom1-2.xx&oh=00_AfCaSspfNHV7L_SDAdus2gJ7LnTkYAk8jkVxmNte-X-dfQ&oe=643FFC85"
+                                    width="200px"
+                                    alt=""
+                                />
+                            </td>
+                            <td>
+                                <h3>"Master Your Finances, Empower Your Future"</h3>
+                                <p>
+                                    Annuity club at IIT Gandhinagar is a financial club that focuses on the concept of annuities, which are a type of financial product that provides a series of payments over a certain period of time. The club aims to educate students about the benefits and drawbacks of annuities and to help them make informed decisions about their personal finances.
+                                </p>
+                                <address>
+                                    <strong>Club Secretary:</strong>
+                                    <br />
+                                    Rahul Chembakessaril
+                                    <br />
+                                    E-mail:{' '}
+                                    <a href="mailto:#" target="_blank">
+                                        16pixels[at]iitgn.ac.in
+                                    </a>
+                                </address>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <Footer />
+        </div>
+    )
+}
+
+export default Clubs
